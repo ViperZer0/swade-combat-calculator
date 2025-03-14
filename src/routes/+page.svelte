4b44cb19b86c3ba4fa2a4e8f2453cedc14922356
ViperZer0 <11594471+ViperZer0@users.noreply.github.com>
2025-03-13 20:15:42 -0700

    Extracted distribution display into its own component
    Refactored stuff so that the configuration for a distribution is
    separate from displaying it.
    
    This is all still just testing stuff and none of these components will
    probably exist in the long term.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
-<script>
-	import TestRoller from "../test-roller/testRoller.svelte";
-    import TestSampler from "../test-sampler/testSampler.svelte";
+<script lang="ts">
+	import { WildDie, type Rollable } from "$lib/dice";
+	import { Option } from "effect";
+	import TestSampler from "../test-sampler/TestSampler.svelte";
+	import { RaiseRollable } from "$lib/skill";
+
+    let sampleSize: number = $state(100);
+    let dieSize: number = $state(4);
+    let testRoller = $derived(new WildDie(dieSize));
+    let dieRoller: Option.Option<Rollable> = $derived(Option.some(new RaiseRollable(new WildDie(dieSize))));
 </script>
 
-<TestSampler />
+<div>
+<label id="diesize">Die Size</label>
+<input type="number" id="diesize" bind:value={dieSize} min="0" />
+</div>
+<div>
+<label for="samplesize">Sample Size</label>
+<input type="number" id="samplesize" bind:value={sampleSize} min="0" />
+</div>
+<TestSampler sampleSize={sampleSize} rollable={dieRoller}/>
--- src/test-sampler/TestSampler.svelte
+++ src/test-sampler/TestSampler.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
-    import { WildDie } from "$lib/dice";
+    import type { Rollable } from "$lib/dice";
 	import { sampleDistribution, SortedSampleDistribution, type Sample } from "$lib/sample";
 	import { Option } from "effect";
     
-    let { dieSize = 4 } = $props();
-    let die = $derived(new WildDie(dieSize));
+    interface Props {
+        sampleSize: number,
+        rollable: Option.Option<Rollable>,
+    };
 
-    let sampleSize = $state(100);
+    let {
+        sampleSize = 0,
+        rollable = Option.none(),
+    }: Props = $props();
+
     let results: Option.Option<SortedSampleDistribution> = $state(Option.none());
-    let total: Option.Option<number> = $derived.by(() => {
-        // I know this looks scary (and it kinda is) but this basically 
-        // just returns None if results hasn't been defined,
-        // and if it HAS been defined it calculates the total
-        // Number of results. This should be the exact same as sampleSize
-        // but maybe we want to double check that???
-        return Option.map(results, (results) => results[Symbol.iterator]().reduce((acc: number, sample: Sample) => acc + sample.count, 0));
-    });
 
     function generateResults() {
-        results = Option.some(sampleDistribution(die, sampleSize).sort());
+        if(Option.isNone(rollable))
+        {
+            results = Option.none();
+            return;
+        }
+        results = Option.some(sampleDistribution(rollable.value, sampleSize).sort());
     }
-
 </script>
 
 <style>
@@ -52,21 +54,12 @@
 
 </style>
 
-<div>
-<label for="dieSize">Die size:</label>
-<input id="dieSize" type="number" bind:value={dieSize} min="2" step="2" />
-</div>
-<div>
-<label for="sampleSize">Sample size:</label>
-<input id="sampleSize" type="number" bind:value={sampleSize} min="1" />
-</div>
 
 <div>
 <button onclick={generateResults}>
     Sample distribution
 </button>
 </div>
-
 <div>
 {#if Option.isSome(results)}
     <table>
@@ -82,9 +75,7 @@
         <tr>
             <td>{result.result}</td>
             <!--<td>{result[1]}</td>-->
-            <!-- We can assert that total should not be None since results is not None.
-            I don't know if there's a better way to logically make these things equivalent? -->
-            <td>{((result.count/Option.getOrThrow(total) * 100).toFixed(2)).toString() + "%"}</td>
+            <td>{((result.count/sampleSize * 100).toFixed(2)).toString() + "%"}</td>
         </tr>
     {/each}
     </tbody>
